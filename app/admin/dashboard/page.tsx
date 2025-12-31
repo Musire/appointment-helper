3b549255ce6bd945abc264baa46250f2fa818bd5
musire <mcmusire@gmail.com>
2025-12-31 11:36:09 -0600

    updated gitignore files
    and tweak - added logout button to the admin dashboard

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -1,8 +1,11 @@
+import { logout } from "@/app/actions/auth.actions";
+
 export default function AdminDashboard () {
   return (
     <div className="page-layout">
         <div className="display-layout py-6">
-            <h1 className="w-full text-3xl capitalize">admin dashboard</h1>
+            <h1 className="text-3xl capitalize w-full text-center">admin dashboard</h1>
+            <button onClick={logout} className="hover:bg-whitesmoke/37 w-24 normal-space hover:cursor-pointer">logout</button>
         </div>
     </div>
   );
