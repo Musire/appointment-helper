33c3a949515ee1782b57f2c02749cd1bb043bdda
musire <mcmusire@gmail.com>
2026-01-28 12:44:55 -0600

    removed unused components, cleaned out folder structure

--- app/admin/components/index.ts
+++ app/admin/components/index.ts
@@ -3,3 +3,4 @@ export { default as ContinueButton } from './ContinueButton';
 export { default as SelectableCard } from './SelectableCard';
 export { default as SelectableDisplay } from './SelectableDisplay';
 export { default as StoreActionTray } from './StoreActionTray';
+export { default as CardDisplay } from "./CardDisplay"
--- components/dashboards/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as CardDisplay } from './CardDisplay';
-export { default as CreateStoreButton } from './CreateStoreButton';
-export { default as StoreCard } from './StoreCard';
-
