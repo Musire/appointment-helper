3489afda2f38272ea902ed344a232fc19c2c5607
musire <mcmusire@gmail.com>
2026-04-06 16:07:53 -0600

    changed the theme button height and padding

--- components/UI/theme/Theme.tsx
+++ components/UI/theme/Theme.tsx
@@ -9,7 +9,7 @@ export default function Theme () {
     const { isDark, toggleTheme } = useThemeState()
     
     return (
-        <button onClick={toggleTheme} className="surface-2 rounded-full h-8 px-1 w-14 flex items-center relative">
+        <button onClick={toggleTheme} className="surface-2 rounded-full h-7 px-1 py-1 w-14 flex items-center relative">
             <div className={clsx(
                 "size-6 blip p-1 centered rounded-full bg-background",
                 {
